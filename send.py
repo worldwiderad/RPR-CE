1565153c1dcdb5ccf93ccc4b4aad5073b71539ac
worldwiderad <51104506+worldwiderad@users.noreply.github.com>
2022-11-12 14:12:20 -0800

    Update send.py

--- send.py
+++ send.py
@@ -10,8 +10,8 @@ logging.basicConfig(level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S',
                     format='%(asctime)-15s - [%(levelname)s] %(module)s: %(message)s',)
 
 parser = argparse.ArgumentParser(description='Sends a decimal code via a 433/315MHz GPIO device')
-# parser.add_argument('code', metavar='CODE', type=int,
-#                     help="Decimal code to send")
+parser.add_argument('code', metavar='CODE', type=int,
+                    help="Decimal code to send")
 parser.add_argument('-g', dest='gpio', type=int, default=17,
                     help="GPIO pin (Default: 17)")
 parser.add_argument('-p', dest='pulselength', type=int, default=None,
